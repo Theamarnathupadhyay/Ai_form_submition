b618a965cb49396ed9364b7e052e2dd39b9c9553
Theamarnathupadhyay <41618805+Theamarnathupadhyay@users.noreply.github.com>
2026-03-30 13:53:12 +0530

    baseurl

--- src/app/services/AI/gemni-ai.ts
+++ src/app/services/AI/gemni-ai.ts
@@ -6,7 +6,8 @@ import { HttpClient } from '@angular/common/http';
 })
 export class GemniAiServices {
 
-  private baseUrl = 'http://localhost:3400/';
+  // private baseUrl = 'http://localhost:3400/';
+private baseUrl = 'https://probable-space-potato-q9wq94xqj55h4vj9-3400.app.github.dev/';
   private gemniAi = 'ai/chat';
 
   private aiChatUrl = `${this.baseUrl}${this.gemniAi}`;
